5c20e354ef52e3661b502605f8858c1f77d6cefb
tracethompson <tracethmpsn@gmail.com>
2020-11-13 21:22:38 -0500

    fix cap

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,5 @@
-import Nav from '../components/Nav'
-import Footer from '../components/Footer'
+import Nav from '../components/nav'
+import Footer from '../components/footer'
 import Head from 'next/head'
 
 export default function IndexPage({children}) { 
